d209ead2d3a58c99b6c5fd593fb24ea3542b44b1
Kadir Çetin <kadir.cetin72@gmail.com>
2026-05-05 10:03:01 +0300

    Implement FeedActivity to show posts

--- app/src/main/java/com/kadircetin/instalite/view/FeedActivity.java
+++ app/src/main/java/com/kadircetin/instalite/view/FeedActivity.java
@@ -1,23 +1,112 @@
 package com.kadircetin.instalite.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.kadircetin.instalite.R;
+import com.kadircetin.instalite.adapter.PostAdapter;
+import com.kadircetin.instalite.databinding.ActivityFeedBinding;
+import com.kadircetin.instalite.model.Post;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Objects;
 
 public class FeedActivity extends AppCompatActivity {
+    private ActivityFeedBinding binding;
+    private FirebaseAuth auth;
+    private FirebaseFirestore firebaseFirestore;
+    private ArrayList<Post> postArrayList;
+    private PostAdapter postAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_feed);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        binding = ActivityFeedBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        settings();
+
+        auth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(DebugAppCheckProviderFactory.getInstance());
+
+        getData();
+        postArrayList = new ArrayList<>();
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        postAdapter = new PostAdapter(postArrayList);
+        binding.recyclerView.setAdapter(postAdapter);
+    }
+
+    public void getData() {
+        firebaseFirestore.collection("Posts")
+                .orderBy("date", Query.Direction.DESCENDING)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (error != null) {
+                            Toast.makeText(FeedActivity.this, error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                        }
+                        if (value != null) {
+                            for (DocumentSnapshot snapshot : value.getDocuments()) {
+                                Map<String, Object> data = snapshot.getData();
+                                assert data != null;
+                                String userEmail = (String) data.get("useremail");
+                                String comment = (String) data.get("comment");
+                                String downloadUrl = (String) data.get("downloadurl");
+                                Post post = new Post(userEmail, comment, downloadUrl);
+                                postArrayList.add(post);
+                            }
+                            postAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.add_post){
+            
+        } else if (item.getItemId() == R.id.signout) {
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void settings() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
